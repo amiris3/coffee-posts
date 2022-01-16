4f5cf3ce12b1632487ade641e6c353d4bed687ec
Lou <lcherensac@ymail.com>
2022-01-16 12:06:38 +0100

    creating Post component and PostItem interface

---
+++ components/Post.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
+import { PostItem } from '../models/PostItem';
+
+const Post: React.FC<PostItem> = ({ content, imgsrc }) => {
+    return (
+        <View style={styles.post}>
+            <Text style={styles.postTitle}>{content}</Text>
+            <Image source={{uri: imgsrc ? imgsrc : ''}} accessibilityLabel="coffee image" style={styles.image} />
+        </View>
+    );
+};
+
+const win = Dimensions.get('window');
+const ratio = win.width / 541;
+const styles = StyleSheet.create({
+    post: {
+        flex: 1,
+        backgroundColor: '#fff',
+        color: '#000',
+        marginBottom: '10%'
+    },
+    postTitle: {
+        fontWeight: '600',
+        paddingBottom: 8
+    },
+    image: {
+        width: win.width - 80,
+        height: 362 * ratio
+    }
+});
+
+
+export default Post;
